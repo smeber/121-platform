3240c1bdf9d20b4d35e89d3e3f911217d35cdf95
Elwin Schmitz <eschmitz@redcross.nl>
2023-10-24 16:25:19 +0200

    refactor: Make Database-port configurable via ENV-variable
    With a fallback-value that was previously (and is currently) hard-coded (so we don't have to change existing environments)

--- services/121-service/ormconfig.ts
+++ services/121-service/ormconfig.ts
@@ -4,7 +4,7 @@ import { DataSourceOptions } from 'typeorm';
 export const ORMConfig: DataSourceOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
-  port: 5432,
+  port: Number(process.env.POSTGRES_PORT) || 5432,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DBNAME,
